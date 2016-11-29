76809a0c22554187d560045d62d57dddfe0c88bb
Achilleas Xypolytos <a.xypolytos@sig.eu>
2016-11-29 12:38:19 +0100

    Comment

--- src/sample/java/project/SampleJavaProject.java
+++ src/sample/java/project/SampleJavaProject.java
@@ -9,6 +9,8 @@ import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.Setter;
 
+// comment
+
 /**
  * The main class of the application. It contains the main() method,
  * the first method called.
