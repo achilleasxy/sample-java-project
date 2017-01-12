2fe15d291ddcaf930a838dfa076d180fb9df0b2c
Achilleas Xypolytos <a.xypolytos@sig.eu>
2017-01-12 16:08:36 +0100

    casd

--- src/sample/java/project/AnotherFile.java
+++ src/sample/java/project/AnotherFile.java
@@ -1,4 +1,6 @@
 public class AnotherFile {
+
+private void method1(){
 System.out.println("New file");
 System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");
 System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");
@@ -7,6 +9,22 @@ System.out.println("New file");System.out.println("New file");System.out.println
 System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");System.out.println("New file");
 }
 
+private void method2(int a, int b, int c. int d, int e, int f, int g, int h, int i, int j, int k, int l){
+	if (true){
+		if(false){
+			if(true){
+				if(false){System.out.print.ln("This should add complexity")
+					}
+				}
+			}
+		}
+
+}
+
+}
+
 //Add comment line
 
 //asdasd
+
+
